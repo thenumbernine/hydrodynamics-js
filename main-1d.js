68da95a670f9e4267c05cf63c3e3d8d81a7da02e
Chris Moore <christopher.e.moore@gmail.com>
2013-12-11 18:54:26 -0700

    moved flux methods into their own file

--- main-1d.js
+++ main-1d.js
@@ -93,33 +93,6 @@ function buildEigenstate(matrix, eigenvalues, eigenvectors, eigenvectorsInverse,
 	mat33invert(eigenvectorsInverse, eigenvectors);
 }
 
-var fluxMethods = {
-	'donor cell' : function(r) { return 0; },
-	'Lax-Wendroff' : function(r) { return 1; },
-	
-	//these two are no good with the Godunov (Riemann) solver
-	'Beam-Warming' : function(r) { return r; },
-	'Fromm' : function(r) { return .5 * (1 + r); },
-
-	//Wikipedia
-	CHARM : function(r) { if (r < 0) return 0; return r*(3*r+1)/((r+1)*(r+1)); },
-	HCUS : function(r) { return Math.max(0, 1.5 * (r + Math.abs(r)) / (r + 2) ); },
-	HQUICK : function(r) { return Math.max(0, 2 * (r + Math.abs(r)) / (r + 3) ); },
-	Koren : function(r) { return Math.max(0, Math.min(2*r, (1 + 2*r)/3 ,2) ); },
-	minmod : function(r) { return Math.max(0, Math.min(r,1) ); },
-	Oshker : function(r) { return Math.max(0, Math.min(r,1.5) ); },	//replace 1.5 with 1 <= beta <= 2	
-	ospre : function(r) { return .5 * (r*r + r) / (r*r + r + 1); },
-	smart : function(r) { return Math.max(0, Math.min(2 * r, .25 + .75 * r, 4)); },
-	Sweby : function(r) { return Math.max(0, Math.min(1.5 * r, 1), Math.min(r, 1.5)); },	//replace 1.5 with 1 <= beta <= 2
-	UMIST : function(r) { return Math.max(0, Math.min(2*r, .75 + .25*r, .25 + .75*r, 2)); },	
-	'van Albada 1' : function(r) { return (r * r + r) / (r * r + 1); },
-	'van Albada 2' : function(r) { return 2 * r / (r * r + 1); },
-	
-	'van Leer' : function(r) { return (r + Math.abs(r)) / (1 + Math.abs(r)); },
-	'monotonized central' : function(r) { return Math.max(0, Math.min(2, .5 * (1 + r), 2 * r)); },
-	superbee : function(r) { return Math.max(0,Math.min(1,2*r),Math.min(2,r)); }
-};
-
 var boundaryMethods = {
 	periodic : function(nx,q) {
 		q[0][0] = q[nx-4][0];
--- main-2d.js
+++ main-2d.js
@@ -270,33 +270,6 @@ var drawToScreenMethods = {
 	}
 };
 
-var fluxMethods = {
-	'donor cell' : function(r) { return 0; },
-	'Lax-Wendroff' : function(r) { return 1; },
-	
-	//these two are no good with the Godunov (Riemann) solver
-	'Beam-Warming' : function(r) { return r; },
-	'Fromm' : function(r) { return .5 * (1 + r); },
-
-	//Wikipedia
-	CHARM : function(r) { if (r < 0) return 0; return r*(3*r+1)/((r+1)*(r+1)); },
-	HCUS : function(r) { return Math.max(0, 1.5 * (r + Math.abs(r)) / (r + 2) ); },
-	HQUICK : function(r) { return Math.max(0, 2 * (r + Math.abs(r)) / (r + 3) ); },
-	Koren : function(r) { return Math.max(0, Math.min(2*r, (1 + 2*r)/3 ,2) ); },
-	minmod : function(r) { return Math.max(0, Math.min(r,1) ); },
-	Oshker : function(r) { return Math.max(0, Math.min(r,1.5) ); },	//replace 1.5 with 1 <= beta <= 2	
-	ospre : function(r) { return .5 * (r*r + r) / (r*r + r + 1); },
-	smart : function(r) { return Math.max(0, Math.min(2 * r, .25 + .75 * r, 4)); },
-	Sweby : function(r) { return Math.max(0, Math.min(1.5 * r, 1), Math.min(r, 1.5)); },	//replace 1.5 with 1 <= beta <= 2
-	UMIST : function(r) { return Math.max(0, Math.min(2*r, .75 + .25*r, .25 + .75*r, 2)); },	
-	'van Albada 1' : function(r) { return (r * r + r) / (r * r + 1); },
-	'van Albada 2' : function(r) { return 2 * r / (r * r + 1); },
-	
-	'van Leer' : function(r) { return (r + Math.abs(r)) / (1 + Math.abs(r)); },
-	'monotonized central' : function(r) { return Math.max(0, Math.min(2, .5 * (1 + r), 2 * r)); },
-	superbee : function(r) { return Math.max(0,Math.min(1,2*r),Math.min(2,r)); }
-};
-
 var boundaryMethods = {
 	none : {top : function() {}, left : function() {}, right : function() {}, bottom : function() {}},
 	periodic : {
@@ -484,8 +457,19 @@ var integrationMethods = {
 				}
 			}
 		},
+		applyExternalForce : function(dt) {
+			for (var j = this.nghost; j < this.nx-this.nghost; ++j) {
+				for (var i = this.nghost; i < this.nx-this.nghost; ++i) {
+					var index = i + this.nx * j;
+					var qIndex = 4 * index;
+					var rho = this.q[0 + qIndex];
+					this.q[3 + qIndex] -= dt * (externalForceX * this.q[1 + qIndex] + externalForceY * this.q[2 + qIndex]);
+					this.q[1 + qIndex] -= dt * rho * externalForceX;
+					this.q[2 + qIndex] -= dt * rho * externalForceY;
+				}
+			}
+		},
 		applyMomentumDiffusion : function(dt) {
-			var externalForce = [externalForceX, externalForceY];
 			for (var j = this.nghost; j < this.nx-this.nghost; ++j) {
 				for (var i = this.nghost; i < this.nx-this.nghost; ++i) {
 					var index = i + this.nx * j;
@@ -504,13 +488,12 @@ var integrationMethods = {
 						var dPressure = pressureR - pressureL;
 						var dx = this.x[side + 2 * indexR] - this.x[side + 2 * indexL];
 						var rho = this.q[0 + qIndex];
-						this.q[1+side + qIndex] -= dt * (dPressure / dx + rho * externalForce[side]);
+						this.q[1+side + qIndex] -= dt * dPressure / dx;
 					}
 				}
 			}
 		},
 		applyWorkDiffusion : function(dt) {
-			var externalForce = [externalForceX, externalForceY];
 			var dxi = [];
 			for (var j = this.nghost; j < this.nx-this.nghost; ++j) {
 				for (var i = this.nghost; i < this.nx-this.nghost; ++i) {
@@ -541,9 +524,8 @@ var integrationMethods = {
 						var pressureR = this.pressure[indexR];
 						if (this.solid[indexR]) pressureR = pressureC;
 						
-						var momentum = this.q[1+side + qIndex];
 						var dx = this.x[side + 2 * indexR] - this.x[side + 2 * indexL];
-						this.q[3 + qIndex] -= dt * ((pressureR * uR - pressureL * uL) / dx + momentum * externalForce[side]);
+						this.q[3 + qIndex] -= dt * (pressureR * uR - pressureL * uL) / dx;
 					}
 				}
 			}
@@ -1004,9 +986,22 @@ var integrationMethods = {
 		},
 		initPressure : function() {
 		},
+		applyExternalForce : function(dt) {
+			integrationMethods['Backward Euler + Gauss Seidel'].initOldQ.call(this);
+			for (var iter = 0; iter < gaussSeidelIterations; ++iter) {
+				for (var j = this.nghost; j < this.nx-this.nghost; ++j) {
+					for (var i = this.nghost; i < this.nx-this.nghost; ++i) {
+						var qIndex = 4 * (i + this.nx * j);
+						var rho = this.q[0 + qIndex];
+						this.q[3 + qIndex] = this.oldQ[3 + qIndex] - dt * (externalForceX * this.q[1 + qIndex] + externalForceY * this.q[2 + qIndex]);
+						this.q[1 + qIndex] = this.oldQ[1 + qIndex] - dt * rho * externalForceX;
+						this.q[2 + qIndex] = this.oldQ[2 + qIndex] - dt * rho * externalForceY;
+					}
+				}
+			}
+		},
 		applyMomentumDiffusion : function(dt) {
 			integrationMethods['Backward Euler + Gauss Seidel'].initOldQ.call(this);
-			var externalForce = [externalForceX, externalForceY];
 			for (var iter = 0; iter < gaussSeidelIterations; ++iter) {
 				var qIndex = 0;
 				var pIndex = 0;
@@ -1035,8 +1030,7 @@ var integrationMethods = {
 							var minusIndex = i - dirs[side][0] + this.nx * (j - dirs[side][1]);
 							var dPressure = this.pressure[plusIndex] - this.pressure[minusIndex];
 							var dx = this.x[side + 2 * plusIndex] - this.x[side + 2 * minusIndex];
-							var rho = this.q[0 + qIndex];
-							this.q[1+side + qIndex] = this.oldQ[1+side + qIndex] - dt * (dPressure / dx + rho * externalForce[side]);
+							this.q[1+side + qIndex] = this.oldQ[1+side + qIndex] - dt * dPressure / dx;
 						}
 					}
 				}
@@ -1078,8 +1072,7 @@ var integrationMethods = {
 							var uL = this.q[1+side + 4 * minusIndex] / this.q[0 + 4 * minusIndex];
 							var pressureR = this.pressure[plusIndex];
 							var pressureL = this.pressure[minusIndex];
-							var momentum = this.q[1+side + qIndex];
-							this.q[3 + qIndex] = this.oldQ[3 + qIndex] - dt * ((pressureR * uR - pressureL * uL) / dx + momentum * externalForce[side]);
+							this.q[3 + qIndex] = this.oldQ[3 + qIndex] - dt * (pressureR * uR - pressureL * uL) / dx;
 						}
 					}
 				}		
@@ -1772,6 +1765,9 @@ var HydroState = makeClass({
 
 		//compute pressure
 		integrationMethods[this.integrationMethod].initPressure.call(this);
+
+		//apply external force
+		integrationMethods[this.integrationMethod].applyExternalForce.call(this, dt);
 
 		//apply momentum diffusion = pressure
 		integrationMethods[this.integrationMethod].applyMomentumDiffusion.call(this, dt);
